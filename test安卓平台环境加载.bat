b69e51fb2df9fa06faab2d40db46f9bfbc9b9959
chrelyonly <1172576293@qq.com>
2026-06-22 21:09:46 +0800

    123123

--- test安卓平台环境加载.bat
+++ test安卓平台环境加载.bat
@@ -1,8 +1,10 @@
+set GOROOT=D:\dev\sdk\go\gopath\pkg\mod\golang.org\toolchain@v0.0.1-go1.26.4.windows-amd64
+set GOPATH=D:\dev\dev\sdk\gopath
 set ANDROID_NDK=D:\dev\sdk\andorid\ndk\28.2.13676358
 set TOOLCHAIN=%ANDROID_NDK%\toolchains\llvm\prebuilt\windows-x86_64
 set GOOS=android
 set GOARCH=amd64
 set API=26
 set CGO_ENABLED=1
-set CC=%TOOLCHAIN%\bin\aarch64-linux-android%API%-clang 
-set CXX=%TOOLCHAIN%\bin\aarch64-linux-android%API%-clang++
+set CC=%TOOLCHAIN%\bin\x86_64-linux-android%API%-clang 
+set CXX=%TOOLCHAIN%\bin\x86_64-linux-android%API%-clang++
